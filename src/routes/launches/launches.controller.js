66e99642ec89b4426d406e2a306065c8e1515c89
Abbracx <tankoraphael@gmail.com>
2022-06-14 15:09:26 +0100

    Validated input request for new launch data

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -10,7 +10,22 @@ function httpGetAllLaunches(req, res){
 
 function httpAddNewLaunch(req, res){
     const launch = req.body
+
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination ){
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate)
+
+    // ALTERNATIVE: if(isNaN(launch.launchDate)) return res.status(400).json({})
+    if(launch.launchDate.toString() === 'Invalid Date'){
+        return res.status(400).json({
+            error: "Invalid launch date"
+        })
+    }
+
     addNewLaunch(launch)
     return res.status(201).json(launch)
 }
