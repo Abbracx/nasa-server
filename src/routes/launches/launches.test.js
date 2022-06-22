107b2e3ca3d98fa8782923ecaa147824671ac41d
Abbracx <tankoraphael@gmail.com>
2022-06-22 03:44:38 +0100

    chore: Setup Supertest for api test

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,21 +1,23 @@
-describe('Test GET /launches', () => {
-    test('It should respond with 200 success', ()=>{
-        const response = 200;
-        expect(response).toBe(200);
-    });
+//supertest is needed to make api request calls for testing
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    // const response = await request(app).get('/launches');
+    // expect(response.statusCode).toBe(200);
+
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
 });
 
+describe("Test POST /launches", () => {
+  test("It should respond with 200 success", () => {});
 
-describe('Test POST /launches', () => {
-    test('It should respond with 200 success', () => {
+  test("It should catch missing required properties", () => {});
 
-    });
-
-    test('It should catch missing required properties', () => {
-
-    });
-
-    test('It should catch invalid dates', () => {
-        
-    });
+  test("It should catch invalid dates", () => {});
 });
